98a52f5c654daab09a2585d00554cb2c7d8630ea
Niels Visbeek <n.visbeek44@gmail.com>
2020-02-19 21:09:02 +0100

    Implemented try-catch for client-side files incase for some reason an error occurs on client-side in production mode.

--- object-streamer-client/client.mjs
+++ object-streamer-client/client.mjs
@@ -10,38 +10,59 @@ let objStreamer = new ObjectStreamer();
 
 // when an object is streamed in
 alt.onServer( "entitySync:create", entity => {
-    if( entity.data ) {
-        let data = entity.data.entityData;
+    try {
+        if( entity.data ) {
+            let data = entity.data.entityData;
 
-        if( data && data.EntityType === "object" ) {
-            objStreamer.add(
-                +entity.id, data.Model, data.EntityType,
-                entity.position.x, entity.position.y, entity.position.z,
-                data.RotX, data.RotY, data.RotZ
-            );
+            if( data && data.EntityType === "object" ) {
+                objStreamer.add(
+                    +entity.id, data.Model, data.EntityType,
+                    entity.position.x, entity.position.y, entity.position.z,
+                    data.RotX, data.RotY, data.RotZ
+                );
+            }
         }
+    } catch( e ) {
     }
 } );
 
 // when an object is streamed out
 alt.onServer( "entitySync:remove", entityId => {
-    objStreamer.remove( +entityId );
+    try {
+        objStreamer.remove( +entityId );
+    } catch( e ) {
+    }
 } );
 
 // when a streamed in object changes position data
 alt.onServer( "entitySync:updatePosition", ( entityId, { x, y, z } ) => {
-    objStreamer.updatePosition( entityId, x, y, z );
+    try {
+        objStreamer.updatePosition( entityId, x, y, z );
+    } catch( e ) {
+    }
 } );
 
 // when a streamed in object changes data
 alt.onServer( "entitySync:updateData", ( entityId, { entityData } ) => {
-    objStreamer.updateData(
-        +entityId,
-        { model: entityData.Model, rx: entityData.RotX, ry: entityData.RotY, rz: entityData.RotZ, entityType: entityData.EntityType }
-    );
+    try {
+        objStreamer.updateData(
+            +entityId,
+            {
+                model: entityData.Model,
+                rx: entityData.RotX,
+                ry: entityData.RotY,
+                rz: entityData.RotZ,
+                entityType: entityData.EntityType
+            }
+        );
+    } catch( e ) {
+    }
 } );
 
 // when a streamed in object needs to be removed
 alt.onServer( "entitySync:clearCache", entityId => {
-    objStreamer.clear( +entityId );
+    try {
+        objStreamer.clear( +entityId );
+    } catch( e ) {
+    }
 } );
--- object-streamer-client/object-streamer.mjs
+++ object-streamer-client/object-streamer.mjs
@@ -20,79 +20,97 @@ export class ObjectStreamer {
 
     add( entityId, model, entityType, x, y, z, rx, ry, rz ) {
 
-        // clear the object incase it still exists.
-        objects.remove( entityId );
-        objects.clear( entityId );
+        try { // clear the object incase it still exists.
+            objects.remove( entityId );
+            objects.clear( entityId );
 
-        let handle = objects.spawnObject( model, x, y, z, rx, ry, rz );
+            let handle = objects.spawnObject( model, x, y, z, rx, ry, rz );
 
-        objects.list.push( {
-            handle: handle, entityId: +entityId, model: model, entityType: entityType,
-            x: x, y: y, z: z, rx: rx, ry: ry, rz: rz
-        } );
+            objects.list.push( {
+                handle: handle, entityId: +entityId, model: model, entityType: entityType,
+                x: x, y: y, z: z, rx: rx, ry: ry, rz: rz
+            } );
+        } catch( e ) {
+        }
     }
 
     spawnObject( model, x, y, z, rx, ry, rz ) {
-        let handle = natives.createObject( natives.getHashKey( model ), x, y, z, true, true, true );
-        natives.setEntityRotation( handle, Math.PI * rx / 180, Math.PI * ry / 180, Math.PI * rz / 180, 1, true );
+        try {
+            let handle = natives.createObject( natives.getHashKey( model ), x, y, z, true, true, true );
+            natives.setEntityRotation( handle, Math.PI * rx / 180, Math.PI * ry / 180, Math.PI * rz / 180, 1, true );
 
-        return handle;
+            return handle;
+        } catch( e ) {
+            return null;
+        }
     }
 
     updatePosition( entityId, x, y, z ) {
-        let idx = objects.list.findIndex( o => +o.entityId === +entityId );
+        try {
+            let idx = objects.list.findIndex( o => +o.entityId === +entityId );
 
-        if( idx === -1 )
-            return;
+            if( idx === -1 )
+                return;
 
-        if( objects.list[ idx ].handle !== null )
-            natives.deleteObject( +objects.list[ idx ].handle );
+            if( objects.list[ idx ].handle !== null )
+                natives.deleteObject( +objects.list[ idx ].handle );
 
-        objects.list[ idx ].handle = null;
-        objects.list[ idx ].handle = objects.spawnObject( objects.list[ idx ].model, x, y, z, objects.list[ idx ].rx, objects.list[ idx ].ry, objects.list[ idx ].rz );
+            objects.list[ idx ].handle = null;
+            objects.list[ idx ].handle = objects.spawnObject( objects.list[ idx ].model, x, y, z, objects.list[ idx ].rx, objects.list[ idx ].ry, objects.list[ idx ].rz );
+        } catch( e ) {
+        }
     }
 
     updateData( entityId, data ) {
-        let idx = objects.list.findIndex( o => +o.entityId === +entityId );
+        try {
+            let idx = objects.list.findIndex( o => +o.entityId === +entityId );
 
-        if( idx === -1 )
-            return;
+            if( idx === -1 )
+                return;
 
-        for( const key in data ) {
-            if( data.hasOwnProperty( key ) )
-                objects.list[ idx ][ key ] = data[ key ];
+            for( const key in data ) {
+                if( data.hasOwnProperty( key ) )
+                    objects.list[ idx ][ key ] = data[ key ];
+            }
+
+            // delete existing object
+            if( objects.list[ idx ].handle !== null )
+                natives.deleteObject( +objects.list[ idx ].handle );
+
+            objects.list[ idx ].handle = null;
+            objects.list[ idx ].handle = objects.spawnObject(
+                objects.list[ idx ].model,
+                objects.list[ idx ].x, objects.list[ idx ].y, objects.list[ idx ].z,
+                objects.list[ idx ].rx, objects.list[ idx ].ry, objects.list[ idx ].rz
+            );
+        } catch( e ) {
         }
-
-        // delete existing object
-        if( objects.list[ idx ].handle !== null )
-            natives.deleteObject( +objects.list[ idx ].handle );
-
-        objects.list[ idx ].handle = null;
-        objects.list[ idx ].handle = objects.spawnObject(
-            objects.list[ idx ].model,
-            objects.list[ idx ].x, objects.list[ idx ].y, objects.list[ idx ].z,
-            objects.list[ idx ].rx, objects.list[ idx ].ry, objects.list[ idx ].rz
-        );
     }
 
     remove( entityId ) {
-        let idx = objects.list.findIndex( o => +o.entityId === +entityId );
+        try {
+            let idx = objects.list.findIndex( o => +o.entityId === +entityId );
 
-        if( idx === -1 )
-            return;
+            if( idx === -1 )
+                return;
 
-        natives.deleteObject( +objects.list[ idx ].handle );
-        objects.list[ idx ].handle = null;
-        alt.log( "objects in list after remove: ", JSON.stringify( objects.list ) );
+            natives.deleteObject( +objects.list[ idx ].handle );
+            objects.list[ idx ].handle = null;
+            alt.log( "objects in list after remove: ", JSON.stringify( objects.list ) );
+        } catch( e ) {
+        }
     }
 
     clear( entityId ) {
-        let idx = objects.list.findIndex( o => +o.entityId === +entityId );
+        try {
+            let idx = objects.list.findIndex( o => +o.entityId === +entityId );
 
-        if( idx === -1 )
-            return;
+            if( idx === -1 )
+                return;
 
-        objects.list.splice( idx, 1 );
-        alt.log( "objects in list after clear: ", JSON.stringify( objects.list ) );
+            objects.list.splice( idx, 1 );
+            alt.log( "objects in list after clear: ", JSON.stringify( objects.list ) );
+        } catch( e ) {
+        }
     }
 }
